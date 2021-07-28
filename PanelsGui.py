9b65bbbd3f1df456c39a0b511938d37dedb6ce69
jeff <jeff@ms4.com>
2021-07-28 15:05:21 -0600

    fix stairs, refactor wall code

--- PanelsGui.py
+++ PanelsGui.py
@@ -34,7 +34,6 @@ import os
 
 __dir__ = os.path.dirname(__file__)
 
-# FreeCAD Command made with a Python script
 def MakeBox(box_len = 1, box_wid = 1, box_hei = 1, box_x = 0, box_y = 0, box_z = 0, box_name='box', box_t=0):
     doc = FreeCAD.ActiveDocument
     box =  doc.addObject("Part::Box",box_name)
@@ -47,21 +46,35 @@ def MakeBox(box_len = 1, box_wid = 1, box_hei = 1, box_x = 0, box_y = 0, box_z =
     Z = FreeCAD.Units.parseQuantity(str(box_z) + 'in')
     box.Placement = FreeCAD.Placement(FreeCAD.Vector(X,Y,Z),FreeCAD.Rotation(FreeCAD.Vector(0,0,1),0))
 
-def MakeStep(step = 0):
-    stepno = str(step + 1)
-    x = 10.5 * step
-    z = 7.5 * step
-    MakeBox(11.25, 42.125, 1.5, x, 0, z+6, "Step_" + stepno)
-    MakeBox(11.25, 42.125, 1.5, x+1.125, 0, z+4.5, "Step_" + stepno + '_Base')
-    MakeBox(0.375, 42.125, 6, x+0.75, 0, z, "Step_" + stepno + '_Front')
+def MakePanel(stud_width, stud_len, panel_width, stud_2_pos, stud_3_pos):
+    MakeBox(panel_width, stud_width, 1.5, 0, 0, 0, "Bottom_Plate")
+    MakeBox(panel_width, stud_width, 1.5, 0, 0, stud_len + 1.5, "Top_Plate")
+    MakeBox(1.5, stud_width, stud_len, 0, 0, 1.5, "Left_Stud")
+    if (stud_2_pos > 0):
+        MakeBox(1.5, stud_width, stud_len, stud_2_pos, 0, 1.5, "Stud_2")
+    if (stud_3_pos > 0):
+        MakeBox(1.5, stud_width, stud_len, stud_3_pos, 0, 1.5, "Stud_3")
+    MakeBox(1.5, stud_width, stud_len, 46.5, 0, 1.5, "Right_Stud")
 
+def MakeBasicPanel(stud_width = 5.5, stud_len = 92.625):
+    MakePanel(stud_width, stud_len, 48, 15.5, 31)
+
+def MakeStep(step = 0):
+    stepno = str(step)
+    x = 10.5 * step
+    z = 7.75 * step
+    MakeBox(11.25, 42.125, 1.5, x, 0, z+3.375, "Step_" + stepno)
+    MakeBox(11.25, 42.125, 1.5, x+1.125, 0, z+1.875, "Step_" + stepno + '_Base')
+    MakeBox(32, 1.5, 3.5, x+1.125, 0, z-1.625, "Step_" + stepno + '_Support1')
+    MakeBox(32, 1.5, 3.5, x+1.125, 40.6252, z-1.625, "Step_" + stepno + '_Support2')
+    MakeBox(0.375, 42.125, 7.25, x+0.75, 0, z-3.875, "Step_" + stepno + '_Front')
 
 class _MakeStairsCmd:
 
     def Activated(self):
-        for step in range(0,15):
+        for step in range(1,15):
             MakeStep(step)
-        MakeBox(0.375, 42.125, 6, 157.5+0.75, 0, 112.5, "Top_Front")
+        MakeBox(0.375, 42.125, 7.25, 15*10.5+0.75, 0, 15*7.75-3.875, "Top_Front")
         FreeCADGui.activeDocument().activeView().viewAxonometric()
         FreeCADGui.SendMsgToActiveView("ViewFit")
 
@@ -80,17 +93,11 @@ class _MakeStairsCmd:
 class _Make8ftPanelCmd:
 
     def Activated(self):
-        # what is done when the command is clicked
-        MakeBox(48, 5.5, 1.5, 0, 0, 0, "Bottom_Plate")
-        MakeBox(48, 5.5, 1.5, 0, 0, 94.125, "Top_Plate")
-        MakeBox(1.5, 5.5, 92.625, 0, 0, 1.5, "Left_Stud")
-        MakeBox(1.5, 5.5, 92.625, 15.5, 0, 1.5, "Stud_2")
-        MakeBox(1.5, 5.5, 92.625, 31, 0, 1.5, "Stud_3")
-        MakeBox(1.5, 5.5, 92.625, 46.5, 0, 1.5, "Right_Stud")
+        MakeBasicPanel(5.5, 92.625)
         MakeBox(14, 1.5, 1.75, 1.5, 4, 10, "Botom_Spacer_1")
         MakeBox(14, 1.5, 1.75, 17, 4, 10, "Botom_Spacer_2")
         MakeBox(14, 1.5, 1.75, 32.5, 4, 10, "Botom_Spacer_3")
-        MakeBox(48, 0.75, 96, 0, -0.75, -0.375, "Exterior Plywood", 50) 
+        MakeBox(48, 0.75, 96, 0, -0.75, -0.375, "Exterior Plywood", 50)
         FreeCADGui.activeDocument().activeView().viewAxonometric()
         FreeCADGui.SendMsgToActiveView("ViewFit")
 
@@ -109,22 +116,17 @@ class _Make8ftPanelCmd:
 class _Make8ftWindowCmd:
 
     def Activated(self):
-        # what is done when the command is clicked
+        MakePanel(5.5, 81.375, 48, 3 ,43.5)
         MakeBox(48, 5.5, 1.5, 0, 0, 0, "Bottom_Plate")
-        MakeBox(48, 5.5, 1.5, 0, 0, 94.125, "Top_Plate")
-        MakeBox(1.5, 5.5, 81.375, 0, 0, 1.5, "Left_Stud")
-        MakeBox(1.5, 5.5, 81.375, 3, 0, 1.5, "Stud_2")
         MakeBox(1.5, 5.5, 19.875, 15.5, 0, 1.5, "Cripple_1")
         MakeBox(1.5, 5.5, 19.875, 31, 0, 1.5, "Cripple_2")
-        MakeBox(1.5, 5.5, 81.375, 43.5, 0, 1.5, "Stud_3")
-        MakeBox(1.5, 5.5, 81.375, 46.5, 0, 1.5, "Right_Stud")
         MakeBox(48, 1.5, 11.25, 0, 0, 82.875, "Header 1")
         MakeBox(48, 1.5, 11.25, 0, 4, 82.875, "Header 2")
         MakeBox(39, 5.5, 1.5, 4.5, 0, 21.375, "Bottom_Frame")
         MakeBox(11, 1.5, 1.75, 4.5, 4, 10, "Botom_Spacer_1")
         MakeBox(14, 1.5, 1.75, 17, 4, 10, "Botom_Spacer_2")
         MakeBox(11, 1.5, 1.75, 32.5, 4, 10, "Botom_Spacer_3")
-        MakeBox(48, 0.75, 96, 0, -0.75, -0.375, "Exterior Plywood", 50) 
+        MakeBox(48, 0.75, 96, 0, -0.75, -0.375, "Exterior Plywood", 50)
         FreeCADGui.activeDocument().activeView().viewAxonometric()
         FreeCADGui.SendMsgToActiveView("ViewFit")
 
@@ -137,22 +139,15 @@ class _Make8ftWindowCmd:
             'ToolTip': 'Create a new 8ft Window Panel'}
 
     def IsActive(self):
-        # The command will be active if there is an active document
         return not FreeCAD.ActiveDocument is None
 
 class _Make8ftInteriorCmd:
 
     def Activated(self):
-        # what is done when the command is clicked
-        MakeBox(48, 3.5, 1.5, 0, 0, 0, "Bottom_Plate")
-        MakeBox(48, 3.5, 1.5, 0, 0, 94.125, "Top_Plate")
-        MakeBox(1.5, 3.5, 92.625, 0, 0, 1.5, "Left_Stud")
-        MakeBox(1.5, 3.5, 92.625, 15.5, 0, 1.5, "Stud_2")
-        MakeBox(1.5, 3.5, 92.625, 31, 0, 1.5, "Stud_3")
-        MakeBox(1.5, 3.5, 92.625, 46.5, 0, 1.5, "Right_Stud")
+        MakeBasicPanel(3.5, 92.625)
         MakeBox(14, 3.5, 1.5, 1.5, 0, 10, "Botom_Spacer_1")
         MakeBox(14, 3.5, 1.5, 17, 0, 10, "Botom_Spacer_2")
-        MakeBox(14, 3.5, 1.5, 32.5, 0, 10, "Botom_Spacer_3") 
+        MakeBox(14, 3.5, 1.5, 32.5, 0, 10, "Botom_Spacer_3")
         FreeCADGui.activeDocument().activeView().viewAxonometric()
         FreeCADGui.SendMsgToActiveView("ViewFit")
 
@@ -165,24 +160,18 @@ class _Make8ftInteriorCmd:
             'ToolTip': 'Create a new 8ft Interior Panel'}
 
     def IsActive(self):
-        # The command will be active if there is an active document
         return not FreeCAD.ActiveDocument is None
 
 class _Make9ftInteriorCmd:
 
     def Activated(self):
-        # what is done when the command is clicked
-        MakeBox(48, 3.5, 1.5, 0, 0, 0, "Bottom_Plate")
-        MakeBox(48, 3.5, 1.5, 0, 0, 106.125, "Top_Plate")
-        MakeBox(1.5, 3.5, 104.625, 0, 0, 1.5, "Left_Stud")
-        MakeBox(1.5, 3.5, 104.625, 15.5, 0, 1.5, "Stud_2")
-        MakeBox(1.5, 3.5, 104.625, 31, 0, 1.5, "Stud_3")
-        MakeBox(1.5, 3.5, 104.625, 46.5, 0, 1.5, "Right_Stud")
-        MakeBox(14, 3.5, 1.5, 1.5, 0, 11.625, "Botom_Spacer_1")
-        MakeBox(14, 3.5, 1.5, 17, 0, 11.625, "Botom_Spacer_2")
-        MakeBox(14, 3.5, 1.5, 32.5, 0, 11.625, "Botom_Spacer_3") 
-        MakeBox(48, 0.375, 96, 0, -0.375, 11.625, "Front Beadboard", 50)
-        MakeBox(48, 0.375, 96, 0, 3.5, 11.625, "Back Beadboard", 50)   
+        #stud length is 1.5 higher than normal
+        MakeBasicPanel(3.5, 107.625)
+        MakeBox(14, 3.5, 1.5, 1.5, 0, 13.125, "Botom_Spacer_1")
+        MakeBox(14, 3.5, 1.5, 17, 0, 13.125, "Botom_Spacer_2")
+        MakeBox(14, 3.5, 1.5, 32.5, 0, 13.125, "Botom_Spacer_3")
+        MakeBox(48, 0.375, 96, 0, -0.375, 13.125, "Front Beadboard", 50)
+        MakeBox(48, 0.375, 96, 0, 3.5, 13.125, "Back Beadboard", 50)
         FreeCADGui.activeDocument().activeView().viewAxonometric()
         FreeCADGui.SendMsgToActiveView("ViewFit")
 
@@ -195,24 +184,18 @@ class _Make9ftInteriorCmd:
             'ToolTip': 'Create a new 9ft 2x4 Interior Panel'}
 
     def IsActive(self):
-        # The command will be active if there is an active document
         return not FreeCAD.ActiveDocument is None
 
 class _Make9ftWideInteriorCmd:
 
     def Activated(self):
-        # what is done when the command is clicked
-        MakeBox(48, 5.5, 1.5, 0, 0, 0, "Bottom_Plate")
-        MakeBox(48, 5.5, 1.5, 0, 0, 106.125, "Top_Plate")
-        MakeBox(1.5, 5.5, 104.625, 0, 0, 1.5, "Left_Stud")
-        MakeBox(1.5, 5.5, 104.625, 15.5, 0, 1.5, "Stud_2")
-        MakeBox(1.5, 5.5, 104.625, 31, 0, 1.5, "Stud_3")
-        MakeBox(1.5, 5.5, 104.625, 46.5, 0, 1.5, "Right_Stud")
-        MakeBox(14, 5.5, 1.5, 1.5, 0, 11.625, "Botom_Spacer_1")
-        MakeBox(14, 5.5, 1.5, 17, 0, 11.625, "Botom_Spacer_2")
-        MakeBox(14, 5.5, 1.5, 32.5, 0, 11.625, "Botom_Spacer_3") 
-        MakeBox(48, 0.375, 96, 0, -0.375, 11.625, "Front Beadboard", 50)
-        MakeBox(48, 0.375, 96, 0, 5.5, 11.625, "Back Beadboard", 50)   
+        #stud length is 1.5 higher than normal
+        MakeBasicPanel(5.5, 106.125)
+        MakeBox(14, 5.5, 1.5, 1.5, 0, 13.125, "Botom_Spacer_1")
+        MakeBox(14, 5.5, 1.5, 17, 0, 13.125, "Botom_Spacer_2")
+        MakeBox(14, 5.5, 1.5, 32.5, 0, 13.125, "Botom_Spacer_3")
+        MakeBox(48, 0.375, 96, 0, -0.375, 13.125, "Front Beadboard", 50)
+        MakeBox(48, 0.375, 96, 0, 5.5, 13.125, "Back Beadboard", 50)
         FreeCADGui.activeDocument().activeView().viewAxonometric()
         FreeCADGui.SendMsgToActiveView("ViewFit")
 
@@ -225,27 +208,20 @@ class _Make9ftWideInteriorCmd:
             'ToolTip': 'Create a new 9ft 2x6 Interior Panel'}
 
     def IsActive(self):
-        # The command will be active if there is an active document
         return not FreeCAD.ActiveDocument is None
 
 
 class _Make9ftPanelCmd:
 
     def Activated(self):
-        # what is done when the command is clicked
-        MakeBox(48, 5.5, 1.5, 0, 0, 0, "Bottom_Plate")
-        MakeBox(48, 5.5, 1.5, 0, 0, 106.125, "Top_Plate")
-        MakeBox(1.5, 5.5, 104.625, 0, 0, 1.5, "Left_Stud")
-        MakeBox(1.5, 5.5, 104.625, 15.5, 0, 1.5, "Stud_2")
-        MakeBox(1.5, 5.5, 104.625, 31, 0, 1.5, "Stud_3")
-        MakeBox(1.5, 5.5, 104.625, 46.5, 0, 1.5, "Right_Stud")
+        MakeBasicPanel(5.5, 104.625)
         MakeBox(14, 1.5, 1.75, 1.5, 4, 11.625, "Botom_Spacer_1")
         MakeBox(14, 1.5, 1.75, 17, 4, 11.625, "Botom_Spacer_2")
-        MakeBox(14, 1.5, 1.75, 32.5, 4, 11.625, "Botom_Spacer_3") 
-        MakeBox(14, 1.5, 1.75, 1.5, 0, 95, "Top_Spacer_1")       
-        MakeBox(14, 1.5, 1.75, 17, 0, 95, "Top_Spacer_2") 
+        MakeBox(14, 1.5, 1.75, 32.5, 4, 11.625, "Botom_Spacer_3")
+        MakeBox(14, 1.5, 1.75, 1.5, 0, 95, "Top_Spacer_1")
+        MakeBox(14, 1.5, 1.75, 17, 0, 95, "Top_Spacer_2")
         MakeBox(14, 1.5, 1.75, 32.5, 0, 95, "Top_Spacer_3")
-        MakeBox(48, 0.75, 96, 0, -0.75, -1, "Exterior Plywood", 50)  
+        MakeBox(48, 0.75, 96, 0, -0.75, -1, "Exterior Plywood", 50)
         FreeCADGui.activeDocument().activeView().viewAxonometric()
         FreeCADGui.SendMsgToActiveView("ViewFit")
 
@@ -258,7 +234,6 @@ class _Make9ftPanelCmd:
             'ToolTip': 'Create a new 9ft Exterior Panel'}
 
     def IsActive(self):
-        # The command will be active if there is an active document
         return not FreeCAD.ActiveDocument is None
 
 FreeCADGui.addCommand('Make_9ft_Exterior', _Make9ftPanelCmd())
@@ -268,4 +243,3 @@ FreeCADGui.addCommand('Make_8ft_Window', _Make8ftWindowCmd())
 FreeCADGui.addCommand('Make_8ft_Exterior', _Make8ftPanelCmd())
 FreeCADGui.addCommand('Make_8ft_Interior', _Make8ftInteriorCmd())
 FreeCADGui.addCommand('Make_Stairs', _MakeStairsCmd())
-
